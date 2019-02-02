07f830807cabb7628ec478e0bac59f9b39834b4f
EdKum <you@example.com>
2019-02-02 11:44:57 +0100

    Calling the Server with HttpClient

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -11,10 +11,36 @@ export class DataService{
 
     baseUrl: string = "assets/";
 
-    constructor(){
+    constructor( private http: HttpClient){}
 
+    getCustomers() : Observable<ICostumer[]>{
+        return this.http.get<ICostumer[]>(this.baseUrl + 'customers.json')
+            .pipe(
+                catchError(this.handleError)
+            );
     }
-
+   
+    getCustomer(id: number) : Observable<ICostumer> {
+        return this.http.get<ICostumer[]>(this.baseUrl + 'customers.json')
+          .pipe(
+            map(customers => {
+              let customer = customers.filter((cust: ICostumer) => cust.id === id);
+              return (customer && customer.length) ? customer[0] : null;
+            }),
+            catchError(this.handleError)
+          )
+      }
+  
+      getOrders(id: number) : Observable<IOrder[]> {
+        return this.http.get<IOrder[]>(this.baseUrl + 'orders.json')
+          .pipe(
+            map(orders => {
+              let custOrders = orders.filter((order: IOrder) => order.customerId === id);
+              return custOrders;
+            }),
+            catchError(this.handleError)
+          );
+      }
 
 
     private handleError(error: any){
